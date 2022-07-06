648433da64e2d7f4705a0be27a1a420af77e7ecb
Amanda Briones <amandabriones111@gmail.com>
2022-07-05 23:35:21 -0700

    commit tree 561e908cb7903ac4bb62ba14703a2ac50c0011e7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const firstTwo = drivers.slice(0,2);
+const lastTwo = drivers.slice(-2);
+
+function returnFirstTwoDrivers() {
+    return firstTwo;
+}
+function returnLastTwoDrivers() {
+    return lastTwo;
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function() {
+        return integer*integer;
+    }
+}
+const fareDoubler = fare => fare*2;
+const fareTripler = fare => fare*3;
+
+const selectDifferentDrivers = (driver, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(driver)
+}
